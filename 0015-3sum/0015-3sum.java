68b5befcccdf36226f8129b69e17502b36cb3a14
Bhanupratap singh <80687399+whybhanu@users.noreply.github.com>
2023-04-08 18:55:32 +0530

    Time: 30 ms (98.56%), Space: 49.9 MB (51.76%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,46 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList();
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            // a + b + c = 0 
+            // b + c = 0 - a
+            // skipping the duplicates
+            if(i == 0 || nums[i - 1] != nums[i]){
+                twoSumSorted(nums, i+1, nums.length-1, -1*nums[i]);
+            }
+
+        }
+        return res;
+            
+    }
+
+    void twoSumSorted(int[] nums, int i, int j, int target){
+        int a1 = nums[i - 1];
+        while(i < j){
+            if(nums[i] + nums[j] > target){
+                j--;
+            } else if(nums[i] + nums[j] < target){
+                i++;
+            } else{
+                List<Integer> list = new ArrayList();
+                list.add(a1);
+                list.add(nums[i]);
+                list.add(nums[j]);
+
+                res.add(list);
+                // for b duplicate
+                while(i < j && nums[i] == nums[i + 1]){
+                    i++;
+                }
+                // for c duplicate
+                while(i < j && nums[j] == nums[j-1]){
+                    j--;
+                }
+                i++;
+                j--;
+            }
+        }
+    }
+}
