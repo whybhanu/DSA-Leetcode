73289e84feca65a3bad75e1d43e2b35c5ad6b726
Bhanupratap singh <80687399+whybhanu@users.noreply.github.com>
2023-04-15 15:26:30 +0530

    Time: 1 ms (100.00%), Space: 53 MB (25.77%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return true;
+        }
+
+        boolean isIncreasing = nums[n - 1] > nums[0];
+        for (int i = 1; i < n; i++) {
+            if (isIncreasing) {
+                if (nums[i - 1] > nums[i]) {
+                    return false;
+                }
+            }
+            else {
+                if (nums[i - 1] < nums[i]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
