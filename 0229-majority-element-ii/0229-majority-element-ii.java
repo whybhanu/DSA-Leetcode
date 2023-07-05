e2d92a52b99a1c5b8450e6d6ed9cea1dba59c590
Bhanupratap singh <80687399+whybhanu@users.noreply.github.com>
2023-07-05 16:58:42 +0530

    Time: 20 ms (5.47%), Space: 46.4 MB (75.57%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,28 +1,15 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        HashSet<Integer> set = new HashSet<>();
-        if(nums.length < 3){
-            for(int i = 0; i < nums.length; i++){
-                set.add(nums[i]);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])) map.put(nums[i], map.get(nums[i]) + 1);
+            else map.put(nums[i], 1);
+            if(map.get(nums[i]) > nums.length/3){
+                if(!list.contains(nums[i])) list.add(nums[i]);
             }
-            ArrayList<Integer> list = new ArrayList<>(set);
-            return list;
+            if(list.size() == 2) break;
         }
-        int n = nums.length/3;
-
-        int count = 1;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i - 1] != nums[i]){
-                count = 1;
-            } else {
-                count++;
-            }
-            if(count > n){
-                set.add(nums[i]);
-            }
-        }
-        ArrayList<Integer> list = new ArrayList<>(set);
         return list;
     }
 }
