644817459d8a40fd82fc4425b2ab13248a615590
Bhanupratap singh <80687399+whybhanu@users.noreply.github.com>
2023-07-08 19:44:04 +0530

    Time: 16 ms (59.03%), Space: 40.4 MB (33.57%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        if(board == null || board.length == 0) return;
+        solve(board);
+    }
+
+    public boolean solve(char[][] board){
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == '.'){
+                    for(char c = '1'; c <= '9'; c++){
+                        if(isValid(board, i, j, c)){
+                            board[i][j] = c;
+                            if(solve(board)) return true;
+                            else board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean isValid(char[][] board, int row, int col, char num){
+        int blkrow = (row / 3) * 3, blkcol = (col / 3) * 3; // Block no. is i/3, first element is i/3*3
+        for (int i = 0; i < 9; i++)
+            if (board[i][col] == num || board[row][i] == num || board[blkrow + i / 3][blkcol + i % 3] == num) return false;
+        return true;
+    }
+}
