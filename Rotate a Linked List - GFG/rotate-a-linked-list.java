c09a1ca53b555803e143975970ed8b85c21fc249
Bhanupratap singh <80687399+whybhanu@users.noreply.github.com>
2023-10-06 00:07:50 +0530

    Added solution - LeetHub

---
+++ Rotate a Linked List - GFG/rotate-a-linked-list.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+            
+            for (int i=0; i<n-1; i++)
+            {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+            
+            int k = sc.nextInt();
+            
+            Solution ob = new Solution();
+            head = ob.rotate(head,k);
+            printList(head);
+        }
+    }
+    
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+/* node of linked list:
+
+class Node{
+    int data;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
+    }
+}
+
+*/
+
+class Solution{
+    public Node rotate(Node head, int k) {
+        if(k == 0) return head;
+        Node curr = head;
+        while(k > 1){
+            curr = curr.next;
+            k--;
+        }
+        if(curr.next == null) return head;
+        Node newNode = curr.next;
+        curr.next = null;
+        Node x = newNode;
+        while(x.next != null){
+            x = x.next;
+        }
+        x.next = head;
+        return newNode;
+    }
+}
